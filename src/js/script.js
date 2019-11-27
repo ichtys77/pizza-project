97d4596ae5c01018370078f017e04b71eda0e3f2
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2019-11-27 09:03:29 +0100

    Add Cart basket hoover

--- src/js/script.js
+++ src/js/script.js
@@ -376,6 +376,7 @@
       thisCart.products = [];
 
       thisCart.getElements(element);
+      thisCart.initActions(element);
 
       console.log('thisCart: ', thisCart);
     }
@@ -384,9 +385,21 @@
       const thisCart = this;
 
       thisCart.dom = [];
-
       thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      console.log('thisCart.dom.toggleTrigger: ', thisCart.dom.toggleTrigger);
     }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+
+      });
+    }
+
   }
 
   const app = {
@@ -405,25 +418,26 @@
       thisApp.data = dataSource;
     },
 
-    init: function () {
-      const thisApp = this;
-      //console.log('*** App starting ***');
-      //console.log('thisApp:', thisApp);
-      //console.log('classNames:', classNames);
-      //console.log('settings:', settings);
-      //console.log('templates:', templates);
-
-      thisApp.initData();
-      thisApp.initMenu();
-      thisApp.initCart();
-    },
-
     initCart: function (){
       const thisApp = this;
 
       const cartElem = document.querySelector(select.containerOf.cart);
       thisApp.cart = new Cart(cartElem);
     },
+
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+      thisApp.initCart();
+    },
+
   };
 
   app.init();
