67280dfd32363554142818cdc7e608aa9688f77e
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2019-12-13 16:22:04 +0100

    Add [partly] get data from API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { select, templates } from '../settings.js';
+import { select, templates, settings } from '../settings.js';
 import { utils } from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -10,6 +10,41 @@ class Booking {
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+
+  }
+
+  getData() {
+    const thisBooking = this;
+
+    const params = {
+      booking: [
+        settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate),
+
+      ],
+
+      eventsCurrent: [
+
+      ],
+
+      eventsRepeat: [
+
+      ],
+
+    };
+
+    console.log('get params: ', params);
+
+    const urls = {
+      booking:       settings.db.url + '/' + settings.db.booking
+                                     + '?' + params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + settings.db.event
+                                     + '?' + params.eventsRepeat.join('&'),
+    };
+
+    console.log('get urls: ', urls);
 
   }
 
